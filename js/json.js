f5c35ca7b58a25a4d2beeea6ff3eb7f41afe1d74
zackw22 <zackw22@gmail.com>
2012-09-26 19:23:58 -0600

    added 20 dummy json item

--- js/json.js
+++ js/json.js
@@ -11,13 +11,85 @@ var json = {
     "workout2": {
        "date": ["Date:", "2012-09-21"],
        "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "220"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "arms"],
+       "reps": ["Reps:", "12"],
+       "comments": ["Comments:","Arms days"]
+    },
+    "workout3": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "220"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "arms"],
+       "reps": ["Reps:", "12"],
+       "comments": ["Comments:","Arms days"]
+    },
+    "workout4": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "220"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "arms"],
+       "reps": ["Reps:", "12"],
+       "comments": ["Comments:","Arms days"]
+    },
+    "workout5": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
        "currentWeight": ["Current Weight:", "215"],
        "sex": ["Sex:", "Male"],
        "workoutType": ["Workout:", "legs"],
        "reps": ["Reps:", "10"],
        "comments": ["Comments:","Lower body days"]
     },
-     "workout3": {
+    "workout6": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "215"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "legs"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Lower body days"]
+    },
+    "workout7": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "215"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "legs"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Lower body days"]
+    },
+    "workout8": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "215"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "legs"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Lower body days"]
+    },
+    "workout9": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "215"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "legs"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Lower body days"]
+    },
+    "workout10": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "215"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "legs"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Lower body days"]
+    },
+     "workout11": {
        "date": ["Date:", "2012-09-21"],
        "name": ['Name:', "Zack"],
        "currentWeight": ["Current Weight:", "210"],
@@ -26,7 +98,79 @@ var json = {
        "reps": ["Reps:", "1"],
        "comments": ["Comments:","Cardio days"]
     },
-      "workout4": {
+     "workout12": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "cardio"],
+       "reps": ["Reps:", "1"],
+       "comments": ["Comments:","Cardio days"]
+    },
+     "workout13": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "cardio"],
+       "reps": ["Reps:", "1"],
+       "comments": ["Comments:","Cardio days"]
+    },
+     "workout14": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "cardio"],
+       "reps": ["Reps:", "1"],
+       "comments": ["Comments:","Cardio days"]
+    },
+     "workout15": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "cardio"],
+       "reps": ["Reps:", "1"],
+       "comments": ["Comments:","Cardio days"]
+    },
+    "workout16": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "back"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Back getting ripped"]
+    },
+    "workout17": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "back"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Back getting ripped"]
+    },
+    "workout18": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "back"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Back getting ripped"]
+    },
+    "workout19": {
+       "date": ["Date:", "2012-09-21"],
+       "name": ['Name:', "Zack"],
+       "currentWeight": ["Current Weight:", "210"],
+       "sex": ["Sex:", "Male"],
+       "workoutType": ["Workout:", "back"],
+       "reps": ["Reps:", "10"],
+       "comments": ["Comments:","Back getting ripped"]
+    },
+    "workout20": {
        "date": ["Date:", "2012-09-21"],
        "name": ['Name:', "Zack"],
        "currentWeight": ["Current Weight:", "210"],
